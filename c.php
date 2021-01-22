04d0075cf8ac4b60855f0844fd90298cafb9b139
mika <mikael.sabalski@gmail.com>
2021-01-22 14:15:18 +0100

    First commit

---
+++ c.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Hello World from php";
+// PHP code goes here
+?>
---
+++ mail.py
@@ -0,0 +1,15 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")
+message = """\
+Subject: "Html Site Is Running!"
+Project is done."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
